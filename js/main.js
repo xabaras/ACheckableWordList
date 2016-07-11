c0c1847540cb38a546bb7683556258243a550362
Paolo Montalto <montypablo@gmail.com>
2016-07-11 11:34:37 +0200

    Added syntax highlighting

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
 	jQuery.get('https://jsonp.afeld.me/?url=https://raw.githubusercontent.com/xabaras/ACheckableWordList/master/README.md', function(data) {
-	   //process text file line by line
-	   var converter = new showdown.Converter(),
+	   	//process text file line by line
+	   	var converter = new showdown.Converter(),
 	   	   html = converter.makeHtml(data);
 	   	   
-   	   	   $('#page').html(html);
+  	   	$('#page').html(html);
+  	   	   
+  	   	$('pre code').each(function(i, block) {
+  	   		hljs.highlightBlock(block);
+		});
 	});
 });
