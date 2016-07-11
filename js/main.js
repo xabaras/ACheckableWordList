ee3d59e05e28cfddf102b55946815d76250f7008
Paolo Montalto <montypablo@gmail.com>
2016-07-11 10:38:37 +0200

    Fixed page loading

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	jQuery.get('http://cors.io/?u=https://raw.githubusercontent.com/xabaras/ACheckableWordList/master/README.md', function(data) {
+	jQuery.get(' https://crossorigin.me/https://raw.githubusercontent.com/xabaras/ACheckableWordList/master/README.md', function(data) {
 	   //process text file line by line
 	   var converter = new showdown.Converter(),
 	   	   html = converter.makeHtml(data);
